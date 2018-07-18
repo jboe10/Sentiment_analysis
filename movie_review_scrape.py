17e6e26c27eb43658eb4d86ae1f6b94571261f9d
jboe10 <jboesurf@gmail.com>
2018-07-18 14:53:17 -0700

    seperated reviews and scores into txt

--- movie_review_scrape.py
+++ movie_review_scrape.py
@@ -2,6 +2,7 @@ import bs4
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
 
+#target webpage
 my_url = 'https://www.imdb.com/title/tt7784604/reviews?ref_=tt_urv'
 
 #open connection and grab page
@@ -15,25 +16,37 @@ page_soup = soup(page_html, "html.parser")
 #grabs each search result
 containers = page_soup.findAll("div",{"class":"lister-item-content"})
 
-filename = "reviews.csv"
-f = open(filename, "w")
-headers = "rating, review"
+#file for written reviews
+filename_reviews = "scrape\\reviews.txt"
+f_scores = open(filename_reviews, "w")
+
+#file for scores/10
+filename_scores = "scrape\scores.txt"
+f_reviews = open(filename_scores, "w")
+
+#counter =1 
 
 for container in containers:
 
+	#written review container
 	review_container = container.findAll("div",{"class":"text show-more__control"})
 	review = review_container[0].text
 
-
+	#score/10 container
 	text = container.findAll(text=True)
 	score = text[8]
 
-	print("score: " + score)
-	print("review: " + review + "\n")
+	#write results to corresponding files
+	if (score != "\n"):
+		f_scores.write(score + "\n")
+		f_reviews.write(review + "\n" + "**" + "\n")
+		#print(str(counter) + "score: " + score)
+		#print(str(counter) + "review: " + review + "\n")
+		#counter += 1
 
-	f.write(score + "," + review +"\n")
-
-f.close()
+#close files
+f_scores.close()
+f_reviews.close()
 
 
 
